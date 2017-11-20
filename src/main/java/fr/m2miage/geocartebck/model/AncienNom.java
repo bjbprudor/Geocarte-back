05459db77fdb0e2b5975cdfddd9e7020afbb869c
BJBPrudor <6IAM54AZe>
2017-11-20 17:22:51 +0100

    Creation des model de l'application

---
+++ src/main/java/fr/m2miage/geocartebck/model/AncienNom.java
@@ -0,0 +1,61 @@
+package fr.m2miage.geocartebck.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ancienNom")
+public class AncienNom implements Serializable
+{
+
+	private static final long serialVersionUID = 2959644193621394587L;
+
+	@Id
+	private int numero;
+
+	@OneToMany
+    Commune commune;
+
+    private String name;
+
+    public int getId()
+    {
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public AncienNom()
+    {
+
+    }
+
+    public AncienNom(int id, String name)
+    {
+    	this.id = id;
+    	this.name = name;
+    }
+    
+    @Override
+    public String toString() 
+    {
+    	return String.format("SE : { id : %s , name : %s }", this.id, this.name);
+    }
+    
+}
